8e7f3c0d29ec4b27fb8f9029d259d7967cd56586
AkessonHenrik <henrik.akesson@heig-vd.ch>
2016-10-05 13:20:16 +0200

    Adjusted AuthentificationFilter

--- src/main/java/ch/heigvd/amt/web/AuthentificationFilter.java
+++ src/main/java/ch/heigvd/amt/web/AuthentificationFilter.java
@@ -19,22 +19,22 @@ public class AuthentificationFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
 
         // If the requested url contains "Login" or "Register", we have to make sure a logged in user cannot access these pages
-        if (request.getRequestURL().toString().contains("Login") || request.getRequestURL().toString().contains("Register")) {
+        if (request.getRequestURL().toString().contains("login") || request.getRequestURL().toString().contains("legister")) {
             if (userManager.isLogged(request.getSession())) {
                 // cannot login or register again
                 request.getRequestDispatcher("/").forward(request, resp);
                 return;
-            }
+            }rc
 
         // If the requested URL contains "Logout", we have to make sure the client is logged in to access that page
-        } else if (request.getRequestURL().toString().contains("Logout")) {
+        } else if (request.getRequestURL().toString().contains("logout")) {
             if (userManager.isLogged(request.getSession()) == false) {
                 //Cannot log out
                 request.getRequestDispatcher("/").forward(request, resp);
                 return;
             }
         // If the requested URL contains "Protected", we have to make sure the client is logged in to access that content
-        } else if(request.getRequestURL().toString().contains("Protected")) {
+        } else if(request.getRequestURL().toString().contains("protected")) {
             if (userManager.compareSessions(request.getSession()) == false) {
                 request.getRequestDispatcher("WEB-INF/pages/NotAllowed.jsp").forward(request, resp);
                 return;
