4264d3a9a6469a8644a3f0896cc68422c142d215
BafS <fabacrans@gmail.com>
2016-10-10 14:11:08 +0200

    Fix index page

--- src/main/java/ch/heigvd/amt/web/controllers/AppServlet.java
+++ src/main/java/ch/heigvd/amt/web/controllers/AppServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "AppServlet", urlPatterns = {"/index"})
+@WebServlet(name = "AppServlet", urlPatterns = {""})
 public class AppServlet extends HttpServlet {
 
     @EJB
