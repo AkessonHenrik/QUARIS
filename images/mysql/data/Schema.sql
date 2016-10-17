6c86724affd77d465c2ae0221a9ba87d675a6b61
BafS <fabacrans@gmail.com>
2016-10-17 16:02:01 +0200

    Add phpmyadmin and mysql, add users database

---
+++ images/mysql/data/Schema.sql
@@ -0,0 +1,35 @@
+-- ---
+-- Create database
+-- ---
+
+DROP SCHEMA IF EXISTS quaris;
+
+CREATE SCHEMA quaris;
+USE quaris;
+
+-- ---
+-- Table 'users'
+--
+-- ---
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  email VARCHAR(64) NOT NULL,
+  username VARCHAR(255),
+  password VARCHAR(512) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- ---
+-- Foreign Keys
+-- ---
+
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE users ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
